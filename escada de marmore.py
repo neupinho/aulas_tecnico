3284c67d0e75d40d5f3dc952d53e1d9510052e68
Neup Chikito <41495377+neupinho@users.noreply.github.com>
2025-03-20 15:49:46 -0300

    atualização de aprimoramento

--- escada de marmore.py
+++ escada de marmore.py
@@ -1,35 +1,51 @@
 
 
 print("----------Olá, Bem vindo a escada de marmore XD---------")
-print("cuidado onde você anda")
-print("temos alguns comandos,diga 'cansei' para sair da escada de marmore")
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade em números: "))
 
-while True:
-    comandos = input()
-    suspeita = False
-    acertei = False
+if idade < 18:
+    print(f'Desculpa {nome}, tu tem idade pra entrar aqui não, da meia volta')
 
-    if "cansei" in comandos:
-        print("aula de Laura agora ne")
-        break
-    if "aloo" in comandos:
-        print("barulhos altos de beijo*")
-        suspeita = True
-    if "achei voce safadinha" in comandos:
-        print("meu deus, essa fernanda e fernando")
-        acertei = True
-    if "leticia" and "leo" in comandos:
-        print("MEU DEUS LETICIA TU NAO ERA SAPATAO????")
-    if "meu deus nao acredito" in comandos:
-        print("miguel e felipe se pegando omgg")
-    if "raquel" and "gabriel" in comandos:
-        print("KKKKK supera esses 2 visse")
-    if "boquinha" and "neto" in comandos:
-        print("é só amizade, abestalhado")
-    if "camisinha" in comandos:
-        print("KKKKKKKK que horror fizeram atos aqui, sem noção")
-    if "fone" in comandos:
-        print("OMGGG ACHEI")
+if idade >= 18:
+    print("cuidado onde você anda")
+    print("temos alguns comandos,diga 'cansei' para sair da escada de marmore")
 
-    if suspeita and acertei == True:
-        print("meu deus eu confirmei minhas suspeitas")
+    while True:
+        comandos = input()
+        suspeita = False
+        acertei = False
+        if "cansei" in comandos:
+            print("aula de Laura agora ne")
+            break
+        if "aloo" in comandos:
+            print("barulhos altos de beijo*")
+            if suspeita == False:
+                suspeita = True
+        if "achei voce safadinha" in comandos:
+            print("meu deus, essa fernanda e fernando")
+            if suspeita == False:
+                suspeita = True
+                acertei = True
+        if "leticia" and "leo" in comandos:
+            print("MEU DEUS LETICIA TU NAO ERA SAPATAO????")
+            acertei = True
+        if "meu deus nao acredito" in comandos:
+            print("acho que achei algo interessante")
+            suspeita = True
+        if "lixo" in comandos:
+            print("lixo ta la na sala")
+            if suspeita == True:
+                suspeita = False
+        if "marta" in comandos:
+            print("que saudades de tia marta")
+        if "camisinha" in comandos:
+            print("KKKKKKKK que horror fizeram atos aqui, sem noção")
+            
+        if "fone" in comandos:
+            print("ACHEI MEU FONE ROUBADO")
+
+        if suspeita and acertei == True:
+            print("eu confirmei minhas suspeitas")
+            suspeita = False
+            acertei = False
